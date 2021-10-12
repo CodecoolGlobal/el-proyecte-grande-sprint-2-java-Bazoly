991f103cfa59386013be42e931db5fc51bb1e6b4
Richard Havalda <havalda.r94@gmail.com>
2021-10-12 10:55:53 +0200

    fix (#16) aux changed to fragment

--- javapeno-erp/src/components/Layout/Layout.js
+++ javapeno-erp/src/components/Layout/Layout.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
 import Footer from '../Navigation/Footer/Footer';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => {
   return (
-    <Aux>
+    <>
       <Toolbar />
       <main>{props.children}</main>
       <Footer />
-    </Aux>
+    </>
   );
 };
 
--- javapeno-erp/src/containers/Javapeno.js
+++ javapeno-erp/src/containers/Javapeno.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Aux from '../hoc/Aux';
 //import classes from './Javapeno.module.css';
 
 const Javapeno = () => {
   return (
-    <Aux>
+    <>
       <h1>CONTENT</h1>
-    </Aux>
+    </>
   );
 };
 

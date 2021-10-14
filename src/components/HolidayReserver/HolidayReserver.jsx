bcf789cc4d1b288324643790ab62e134c774355c
Richard Havalda <havalda.r94@gmail.com>
2021-10-14 16:13:48 +0200

    feat (#30): implement basic reservation system

--- src/components/HolidayReserver/HolidayReserver.jsx
+++ src/components/HolidayReserver/HolidayReserver.jsx
@@ -1,10 +1,14 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useParams } from 'react-router-dom';
+import Calendar from 'react-calendar';
 
 let HolidayReserver = () => {
   let [userData, setData] = useState([]);
   const { userId } = useParams();
+  const [value, onChange] = useState(new Date());
+  const [from, setFrom] = useState(null);
+  const [to, setTo] = useState(null);
 
   let getUserURL = 'http://localhost:8080/api/user/';
 
@@ -27,6 +31,55 @@ let HolidayReserver = () => {
     <div className='employee'>
       <h1>Holiday</h1>
       <h2>Reserve holiday for: {userData.name}</h2>
+      <h2>From:</h2>
+      {from ? (
+        <div>
+          <p>{from.toDateString()} </p>
+          <h2>To:</h2>
+        </div>
+      ) : (
+        <Calendar
+          onChange={onChange}
+          value={value}
+          onClickDay={(value, event) => {
+            setFrom(value);
+          }}
+        />
+      )}
+      {from ? (
+        to ? (
+          <div>
+            <p>{to.toDateString()} </p>
+            <button
+              onClick={() => {
+                setFrom(null);
+                setTo(null);
+              }}
+            >
+              Modify
+            </button>
+            <button
+              onClick={() => {
+                alert(
+                  `You set a holiday for ${
+                    userData.name
+                  }\nFrom: ${from.toDateString()}, to: ${to.toDateString()}`
+                );
+              }}
+            >
+              Send reservation
+            </button>
+          </div>
+        ) : (
+          <Calendar
+            onChange={onChange}
+            value={value}
+            onClickDay={(value, event) => {
+              setTo(value);
+            }}
+          />
+        )
+      ) : null}
     </div>
   );
 };
